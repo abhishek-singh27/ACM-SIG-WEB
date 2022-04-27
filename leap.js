68621e406d646f0393b9962e2b8615a5bfd067cb
Abhishek Singh <84329948+abhishek-singh27@users.noreply.github.com>
2022-04-27 22:15:01 +0530

    Delete leap.js

--- leap.js
+++
@@ -1,7 +0,0 @@
-let year = prompt("Enter the year to check for");
-if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-        console.log("year " + year + " is a leap year");
-}
-else {
-    console.log("year " + year + " is not a leap year");
-}
